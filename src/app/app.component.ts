cf7937fdf9c4cd6031845f2cbd387a9948b245eb
rtrm <rtrm@hcuge.ch>
2020-04-01 13:48:30 +0200

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,8 +10,8 @@ export class AppComponent implements OnInit {
   private peerConnections = [];
   public peerConnection: any;
 
-  @ViewChild('video') public video: ElementRef;
-  @ViewChild('videoClient') public video2: ElementRef;
+  @ViewChild('myVideo') public myVideo: ElementRef;
+  @ViewChild('videoCandidate') public candidateVideo: ElementRef;
 
   constructor(private socketService: SocketService) { }
 
@@ -22,66 +22,23 @@ export class AppComponent implements OnInit {
       audio: false,
     })
     .then((stream) => {
-      this.video.nativeElement.srcObject = stream;
+      this.myVideo.nativeElement.srcObject = stream;
+
+      // Pass video element to service
+      this.socketService.init(this.myVideo.nativeElement, this.candidateVideo.nativeElement);
+
       this.socketService.socket.emit('broadcaster');
     }).catch(error => console.error(error));
 
-    this.socketService.socket.on('answer', (id, description) => {
-      this.peerConnections[id].setRemoteDescription(description);
-    });
-
-    this.socketService.socket.on('watcher', (id) => {
-      const peerConnection = new RTCPeerConnection();
-      this.peerConnections[id] = peerConnection;
-      const stream = this.video.nativeElement.srcObject;
-      stream.getTracks().forEach(track => peerConnection.addTrack(track, stream));
-
-      peerConnection.createOffer()
-      .then(sdp => peerConnection.setLocalDescription(sdp))
-      .then(() => {
-        this.socketService.socket.emit('offer', id, peerConnection.localDescription);
-      });
-      peerConnection.onicecandidate = (event) => {
-        if (event.candidate) {
-          this.socketService.socket.emit('candidate', id, event.candidate);
-        }
-      };
-    });
-
-    this.socketService.socket.on('offer', (id, description) => {
-      this.peerConnection = new RTCPeerConnection();
-      this.peerConnection.setRemoteDescription(description)
-      .then(() => this.peerConnection.createAnswer())
-      .then(sdp => this.peerConnection.setLocalDescription(sdp))
-      .then(() => {
-        this.socketService.socket.emit('answer', id, this.peerConnection.localDescription);
-      });
-      this.peerConnection.ontrack = (event) => {
-        this.video2.nativeElement.srcObject = event.streams[0];
-      };
-      this.peerConnection.onicecandidate = (event) => {
-        if (event.candidate) {
-          this.socketService.socket.emit('candidate', id, event.candidate);
-        }
-      };
-    });
-
-    this.socketService.socket.on('connect', () => {
-      this.socketService.socket.emit('watcher');
-    });
-
-    this.socketService.socket.on('broadcaster', () => {
-      this.socketService.socket.emit('watcher');
-    });
 
     // this.socketService.socket.on('candidate', (id, candidate) => {
     //   this.peerConnections[id].addIceCandidate(new RTCIceCandidate(candidate));
     // });
 
-    this.socketService.socket.on('bye', (id) => {
-      if (this.peerConnections[id]) { this.peerConnections[id].close(); }
-      delete this.peerConnections[id];
-    });
+    // this.socketService.socket.on('bye', (id) => {
+    //   if (this.peerConnections[id]) { this.peerConnections[id].close(); }
+    //   delete this.peerConnections[id];
+    // });
   }
 
 }
--- src/app/socket.service.ts
+++ src/app/socket.service.ts
@@ -1,11 +1,76 @@
 import { Injectable } from '@angular/core';
 import * as io from 'socket.io-client';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class SocketService {
-  public socket = io('http://rtc.remy-trompier.ch:4000');
+    public socket;
+    private candidateConnection: RTCPeerConnection;
+    private peerConnections = {};
 
-  constructor() {}
+
+    private myVideo: HTMLVideoElement;
+    private candidateVideo: HTMLVideoElement;
+
+    constructor() {
+        // this.socket = io('http://rtc.remy-trompier.ch:4000');
+    }
+
+    public init(myVideo: HTMLVideoElement, candidateVideo: HTMLVideoElement) {
+        this.socket = io('http://localhost:4000');
+        this.socket.on('answer', this.answerHandler.bind(this));
+        this.socket.on('watcher', this.watcherHandler.bind(this));
+        this.socket.on('offer', this.offerHandler.bind(this));
+        this.socket.on('connect', this.connectHandler.bind(this));
+        this.socket.on('broadcaster', this.connectHandler.bind(this));
+
+        this.myVideo = myVideo;
+        this.candidateVideo = candidateVideo;
+    }
+
+    private answerHandler(id, description) {
+        this.peerConnections[id].setRemoteDescription(description);
+    }
+
+    private watcherHandler(id, description) {
+        const peerConnection = new RTCPeerConnection();
+        this.peerConnections[id] = peerConnection;
+        const stream = this.myVideo.srcObject as MediaStream;
+        stream.getTracks().forEach(track => peerConnection.addTrack(track, stream));
+
+        peerConnection.createOffer()
+            .then(sdp => peerConnection.setLocalDescription(sdp))
+            .then(() => {
+                this.socket.emit('offer', id, peerConnection.localDescription);
+            });
+        peerConnection.onicecandidate = (event) => {
+            if (event.candidate) {
+                this.socket.emit('candidate', id, event.candidate);
+            }
+        };
+    }
+
+    private offerHandler(id, description) {
+        this.candidateConnection = new RTCPeerConnection();
+        this.candidateConnection.setRemoteDescription(description)
+            .then(() => this.candidateConnection.createAnswer())
+            .then(sdp => this.candidateConnection.setLocalDescription(sdp))
+            .then(() => {
+                this.socket.emit('answer', id, this.candidateConnection.localDescription);
+            });
+        this.candidateConnection.ontrack = (event) => {
+            this.candidateVideo.srcObject = event.streams[0];
+        };
+        this.candidateConnection.onicecandidate = (event) => {
+            if (event.candidate) {
+                this.socket.emit('candidate', id, event.candidate);
+            }
+        };
+    }
+
+    private connectHandler() {
+        this.socket.emit('watcher');
+    }
+
 
 
 }

dcef4eb716d657b8a1fe1304065e1d3c83e3d52b
FLAHOU Maxime <maxime.flahou@hcuge.ch>
2020-04-01 11:45:15 +0200

    refactor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,8 @@ import { SocketService } from './socket.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  private peerConnections = [];
-  public peerConnection: any;
+
+  public videoStreams = this.socketService.videoStreams;
 
   @ViewChild('video') public video: ElementRef;
   @ViewChild('videoClient') public video2: ElementRef;
@@ -17,71 +17,7 @@ export class AppComponent implements OnInit {
 
   public ngOnInit() {
 
-    navigator.mediaDevices.getUserMedia({
-      video: true,
-      audio: false,
-    })
-    .then((stream) => {
-      this.video.nativeElement.srcObject = stream;
-      this.socketService.socket.emit('broadcaster');
-    }).catch(error => console.error(error));
-
-    this.socketService.socket.on('answer', (id, description) => {
-      this.peerConnections[id].setRemoteDescription(description);
-    });
-
-    this.socketService.socket.on('watcher', (id) => {
-      const peerConnection = new RTCPeerConnection();
-      this.peerConnections[id] = peerConnection;
-      const stream = this.video.nativeElement.srcObject;
-      stream.getTracks().forEach(track => peerConnection.addTrack(track, stream));
-
-      peerConnection.createOffer()
-      .then(sdp => peerConnection.setLocalDescription(sdp))
-      .then(() => {
-        this.socketService.socket.emit('offer', id, peerConnection.localDescription);
-      });
-      peerConnection.onicecandidate = (event) => {
-        if (event.candidate) {
-          this.socketService.socket.emit('candidate', id, event.candidate);
-        }
-      };
-    });
-
-    this.socketService.socket.on('offer', (id, description) => {
-      this.peerConnection = new RTCPeerConnection();
-      this.peerConnection.setRemoteDescription(description)
-      .then(() => this.peerConnection.createAnswer())
-      .then(sdp => this.peerConnection.setLocalDescription(sdp))
-      .then(() => {
-        this.socketService.socket.emit('answer', id, this.peerConnection.localDescription);
-      });
-      this.peerConnection.ontrack = (event) => {
-        this.video2.nativeElement.srcObject = event.streams[0];
-      };
-      this.peerConnection.onicecandidate = (event) => {
-        if (event.candidate) {
-          this.socketService.socket.emit('candidate', id, event.candidate);
-        }
-      };
-    });
-
-    this.socketService.socket.on('connect', () => {
-      this.socketService.socket.emit('watcher');
-    });
-
-    this.socketService.socket.on('broadcaster', () => {
-      this.socketService.socket.emit('watcher');
-    });
-
-    // this.socketService.socket.on('candidate', (id, candidate) => {
-    //   this.peerConnections[id].addIceCandidate(new RTCIceCandidate(candidate));
-    // });
-
-    this.socketService.socket.on('bye', (id) => {
-      if (this.peerConnections[id]) { this.peerConnections[id].close(); }
-      delete this.peerConnections[id];
-    });
+    this.socketService.initConnection();
   }
 
 }
--- src/app/socket.service.ts
+++ src/app/socket.service.ts
@@ -1,11 +1,91 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
 import * as io from 'socket.io-client';
 
 @Injectable({providedIn: 'root'})
 export class SocketService {
-  public socket = io('http://rtc.remy-trompier.ch:4000');
+  public peerConnections = [];
+  // public socket = io('http://rtc.remy-trompier.ch:4000');
+  public socket = io('http://mac-0522.huge.ad.hcuge.ch:4000');
+
+  public videoStreams = new Map<string, MediaStream>();
 
   constructor() {}
 
+
+  public initConnection() {
+    navigator.mediaDevices.getUserMedia({
+      video: true,
+      audio: false,
+    })
+    .then((stream) => {
+      this.videoStreams.set('broadcaster', stream);
+      this.socket.emit('broadcaster');
+    }).catch(error => console.error(error));
+
+    this.socket.on('answer', (id, description) => {
+      this.peerConnections[id].setRemoteDescription(description);
+    });
+
+    this.socket.on('watcher', (id) => {
+      const peerConnection = new RTCPeerConnection({
+        iceServers: [{
+          urls: ['stun:stun.l.google.com:19302']
+        }]
+      });
+      this.peerConnections[id] = peerConnection;
+      const stream = this.videoStreams.get('broadcaster');
+      stream.getTracks().forEach(track => peerConnection.addTrack(track, stream));
+
+      peerConnection.createOffer()
+      .then(sdp => peerConnection.setLocalDescription(sdp))
+      .then(() => {
+        this.socket.emit('offer', id, peerConnection.localDescription);
+      });
+      peerConnection.onicecandidate = (event) => {
+        if (event.candidate) {
+          this.socket.emit('candidate', id, event.candidate);
+        }
+      };
+    });
+
+    this.socket.on('offer', (id, description) => {
+      const peerConnection = new RTCPeerConnection({
+        iceServers: [{
+          urls: ['stun:stun.l.google.com:19302']
+        }]
+      });
+      peerConnection.setRemoteDescription(description)
+      .then(() => peerConnection.createAnswer())
+      .then(sdp => peerConnection.setLocalDescription(sdp))
+      .then(() => {
+        this.socket.emit('answer', id, peerConnection.localDescription);
+      });
+      peerConnection.ontrack = (event) => {
+        this.videoStreams.set(id, event.streams[0]);
+      };
+      peerConnection.onicecandidate = (event) => {
+        if (event.candidate) {
+          this.socket.emit('candidate', id, event.candidate);
+        }
+      };
+    });
+
+    this.socket.on('connect', () => {
+      this.socket.emit('watcher');
+    });
+
+    this.socket.on('broadcaster', () => {
+      this.socket.emit('watcher');
+    });
+
+    // this.socket.on('candidate', (id, candidate) => {
+    //   this.peerConnections[id].addIceCandidate(new RTCIceCandidate(candidate));
+    // });
+
+    this.socket.on('bye', (id) => {
+      if (this.peerConnections[id]) { this.peerConnections[id].close(); }
+      delete this.peerConnections[id];
+    });
+  }
 
 }
